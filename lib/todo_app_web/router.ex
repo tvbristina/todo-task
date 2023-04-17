668ebc0a9c367282c4d3a1acb8fc2617f5dee65c
tvbristina <bristina@techvoot.com>
2023-04-17 10:41:45 +0530

    Added tasks route

--- lib/todo_app_web/router.ex
+++ lib/todo_app_web/router.ex
@@ -17,6 +17,7 @@ defmodule TodoAppWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/tasks", TaskController
   end
 
   # Other scopes may use custom stacks.
